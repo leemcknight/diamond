9f6d8f62d78d6f562f5bb5a252f387f73cf9a727
Lee McKnight <lee_mcknight@hotmail.com>
2020-05-23 10:00:30 -0500

    script updates; gamelogs load now

--- scripts/dynamodb/gamelog.py
+++ scripts/dynamodb/gamelog.py
@@ -1,13 +1,32 @@
 import sys;
+import boto3;
 
+dynamo = boto3.resource('dynamodb')
 
-def buildkey():
+def no_quotes(s):
+    return s.replace('"','')
 
+def buildkey(data):
+    parts = data.split(',')
+    date = no_quotes(parts[0])
+    home_team = no_quotes(parts[6]) 
+    game_number = no_quotes(parts[1]) 
+    return home_team + date + game_number
+
+def load_file(path):
+    table = dynamo.Table('game')
+    with open(path) as gamelogs:
+        for gamelog in gamelogs:
+            log_row = {}
+            log_row['game_id'] = buildkey(gamelog)
+            log_row['log'] = no_quotes(gamelog)
+            table.put_item(Item=log_row)
+            print(log_row['game_id'] + ":" + log_row['log'])
 
 
 def main():
     path = sys.argv[1]
-    load_dir(path)
+    load_file(path)
         
 if __name__ == "__main__":
     main()
