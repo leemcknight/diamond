32f500a61ebe59aee5abd678f9409e1b828b722c
Lee McKnight <lee_mcknight@hotmail.com>
2020-05-24 20:59:43 -0500

    more work on event emitters

--- scripts/stats/EventEmitter.py
+++ scripts/stats/EventEmitter.py
@@ -1,12 +1,46 @@
 import asyncio
 
+
 class EventEmitter:
-	def emit_pitch(self, pitch):
+	emitters = {
+		'B': self.emit_pitch,
+		'C': self.emit_pitch,
+		'F': self.emit_pitch,
+		'H': self.emit_pitch,
+		'I': self.emit_pitch,
+		'K': self.emit_pitch,
+		'L': self.emit_pitch,
+		'M': self.emit_pitch,
+		'N': self.emit_pitch,
+		'O': self.emit_pitch,
+		'P': self.emit_pitch,
+		'Q': self.emit_pitch,
+		'R': self.emit_pitch,
+		'S': self.emit_pitch,
+		'T': self.emit_pitch,
+		'U': self.emit_pitch,
+		'V': self.emit_pitch,
+		'X': self.emit_pitch,
+		'Y': self.emit_pitch,
+		}
+	def emit_pitch(self, pitch, fieldstate):
 		print('emitting pitch: {}'.format(pitch))
 
-	def emit_events(self, event_string):
-		parts = event_string.split(',')
-		if parts[0] == 'play': 
-				pitches = parts[5]
-				for pitch in pitches:
-					self.emit_pitch(pitch) 
+	def emit_advance(self, advance, fieldstate):
+		print('emitting advance: {}'.format(advance))
+
+	def emit_play(self, play, fieldstate):
+		print('emitting play: {}'.format(play))
+
+	def emit_batter(self, batter, fieldstate):
+		print('batter is now: {}'.format(batter)
+
+	def emit_events(self, event_data, fieldstate):
+		emit_batter(event_data[3], fieldstate)
+		pitches = event_data[5]
+		for pitch in pitches:
+			emitters[pitch](pitch, fieldstate)
+			#self.emit_pitch(pitch, fieldstate) 
+		
+		play = event_data[6].split('/')[0]
+		self.emit_play(play, fieldstate)
--- scripts/stats/stats.py
+++ scripts/stats/stats.py
@@ -4,12 +4,55 @@ from os.path import isfile, join
 from EventEmitter import EventEmitter
 
 emitter = EventEmitter()
+fieldstate = {
+		'players': {
+			'0': {},
+			'1': {},
+		'runners': {}
+	}
+}
+
+def start(data):
+	player = data[1]
+	team = data[3]
+	pos = data[5]
+	fieldstate['players'][team][pos] = player
+	print("start: {}".format(data))
+	
+
+def sub(data):
+	print("sub: {}".format(data))
+
+def play(data):
+	emitter.emit_events(data, fieldstate)
+
+def id(data):
+	fieldstate['game_id'] = data[1]
+
+handlers = {
+	'play': play,
+	'start': start,
+	'sub': sub,
+	'id': id,
+	'info': lambda data : None,
+	'version': lambda data : None,
+	'data': lambda data: None,
+	'com': lambda data: None
+}
+
+
+
+def update_context(line):
+	data = line.split(',')
+	context_handlers[data[0]](line)
 
 def process_file(file):
 	print("processing {}".format(file))
 	with open(file, 'r') as event_file:
 		for event_line in event_file:
-			emitter.emit_events(event_line)
+			data = event_line.split(',')
+			print("getting handler for {}".format(data[0]))			
+			handlers[data[0]](data)
 
 def process_dir(path):
 	onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
